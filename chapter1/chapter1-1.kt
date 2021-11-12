ec19cd32ce34cd4535b93190e9fc714897ff0f3f
Yeojong <yeojong@livebarn.com>
2021-11-11 22:38:53 -0500

    add two solution of chapter1-1

--- chapter1/chapter1-1.kt
+++ chapter1/chapter1-1.kt
@@ -21,9 +21,38 @@ fun isUnique(string: String): Boolean {
     return true
 }
 
+fun isUnique2(string: String): Boolean {
+    val charArray = string.toCharArray()
+    val set = mutableSetOf<Char>()
+    for (char in charArray) {
+        if (set.contains(char)) {
+            println("Duplicated2 char is $char")
+            return false
+        } else {
+            set.add(char)
+        }
+    }
+    return true
+}
+
+fun isUnique3(string: String): Boolean {
+    val charArray = string.toCharArray()
+    charArray.sort()
+    for ((index, char) in charArray.withIndex()) {
+        if (index == 0) continue
+        if (char == charArray[index.minus(1)]) {
+            println("Duplicated3 char is $char")
+            return false
+        }
+    }
+    return true
+}
+
 fun test() {
     var wordList = "ll, punch, guess, dance, or, constraint, fraud, nest, leg, photograph, obese, revival, technique, company, performance, sniff, rough, close, budge, process, single, motorist, generate, noble, mislead, wealth, pill, cattle, extinct, aluminium, similar, perception, lock, torture, palm, location, topple, heaven, road, appetite, courage, conscience, wrong, barrel, cause, exclusive, inflate, kidney, expansion, contain, distant".split(", ")
     for (word in wordList) {
         println("isUnique: $word -> ${isUnique(word)}")
+        println("isUnique2: $word -> ${isUnique2(word)}")
+        println("isUnique3: $word -> ${isUnique3(word)}")
     }
 }
